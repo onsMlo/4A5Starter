2b99befe6b2d912a8fe2d215b3ec13390cc6ce1d
onsMlo <110187857+onsMlo@users.noreply.github.com>
2024-02-06 00:35:15 -0500

    add files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Bonjour</h1>
+      <h2>Ons Mlouki</h2>
+    </div>
+  );
+}
